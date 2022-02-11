409a59bcec77ab81ea5c6d498b8543f46bd743e8
optiX <55358375+traneric89@users.noreply.github.com>
2022-02-11 16:11:48 -0500

    restart functionality and board population

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 let gameStarted = 0;
-
 let playerXTurn = 1;
-let playerOTurn = 0;
+let winnerArrayX = [
+  ["X", "X", "X"],
+  ["X", "X", "X"],
+  ["X", "X", "X"],
+];
+let winnerArrayO = [
+  ["O", "O", "O"],
+  ["O", "O", "O"],
+  ["O", "O", "O"],
+];
+let gameArray = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
 
-let cellVariable = document.getElementById("cell");
+const playerTurn = (index) => {
+  if (playerXTurn) {
+    document.getElementById(`cell${index}`).innerHTML = "X";
+    gameArray[Math.floor(index / 3)][index % 3] = "X";
+    playerXTurn = 0;
+    console.table(gameArray);
+  } else {
+    document.getElementById(`cell${index}`).innerHTML = "O";
+    gameArray[Math.floor(index / 3)][index % 3] = "O";
+    playerXTurn = 1;
+    console.table(gameArray);
+  }
+};
 
-const playerTurn = (index) => {};
+const restartGame = () => {
+  let max = 0;
+  while (max <= 8) {
+    document.getElementById(`cell${max}`).innerHTML = "";
+    max++;
+  }
+  playerXTurn = 1;
+  gameArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+};
+
+const gameCheck = () => {};
