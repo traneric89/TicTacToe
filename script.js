70f0ec5185f6e34da611cec39735bf754d127b5a
optiX <55358375+traneric89@users.noreply.github.com>
2022-02-13 00:19:53 -0500

    update new winner detection algorithm

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let gameStarted = 0;
 let playerXTurn = 1;
 let arrayX = [];
 let arrayO = [];
+let sortedCellsX = "";
+let sortedCellsO = "";
 let gameArray = [
   [0, 0, 0],
   [0, 0, 0],
@@ -16,16 +18,12 @@ const playerTurn = (index) => {
     arrayX.push(index);
     winCheck();
     playerXTurn = 0;
-    // console.table(gameArray);
-    // console.log(arrayX);
   } else if (!playerXTurn && gameArray[Math.floor(index / 3)][index % 3] == 0) {
     document.getElementById(`cell${index}`).innerHTML = "O";
     gameArray[Math.floor(index / 3)][index % 3] = `O${index}`;
     arrayO.push(index);
     winCheck();
     playerXTurn = 1;
-    // console.table(gameArray);
-    // console.log(arrayO);
   }
 };
 
@@ -41,49 +39,79 @@ const restartGame = () => {
     [0, 0, 0],
     [0, 0, 0],
   ];
+  arrayX = [];
+  arrayO = [];
+  console.log(arrayX);
+  console.log(arrayO);
 };
 
 const winCheck = () => {
-  let sortedCellsX = arrayX
+  sortedCellsX = arrayX
     .sort((a, b) => a - b)
     .toString()
     .replace(/,/g, "");
-  let sortedCellsO = arrayO
+  sortedCellsO = arrayO
     .sort((a, b) => a - b)
     .toString()
     .replace(/,/g, "");
 
-  console.log(sortedCellsO);
-  console.log(sortedCellsX);
+  // console.log(sortedCellsO);
+  // console.log(sortedCellsX);
 
   if (playerXTurn == 1) {
-    if (sortedCellsX.length <= 3) {
-      winAlgo(sortedCellsX);
-    } else if (sortedCellsX.length == 4) {
-      //   console.log(sortedCellsX);
-      winAlgo(sortedCellsX.substring(0, 3));
-      //   console.log(sortedCellsX.substring(0, 2));
-      winAlgo(sortedCellsX.substring(1, 3));
-      //   console.log(sortedCellsX.substring(1, 3));
-    } else if (sortedCellsX.length == 5) {
-      winAlgo(sortedCellsX.substring(0, 3));
-      winAlgo(sortedCellsX.substring(1, 3));
-      winAlgo(sortedCellsX.substring(2, 3));
-    }
+    winAlgo(sortedCellsX);
+    console.log(arrayX);
+    console.log(sortedCellsX);
+    // if (sortedCellsX.length <= 3) {
+    //   winAlgo(sortedCellsX);
+    //   console.log(sortedCellsX);
+    // } else if (sortedCellsX.length == 4) {
+    //   // winAlgo(sortedCellsX.substring(0, 3));
+    //   // winAlgo(sortedCellsX.substring(1, 3));
+    //   console.log(sortedCellsX);
+    // } else if (sortedCellsX.length == 5) {
+    //   // winAlgo(sortedCellsX.substring(0, 3));
+    //   // winAlgo(sortedCellsX.substring(1, 3));
+    //   // winAlgo(sortedCellsX.substring(2, 3));
+    //   console.log(sortedCellsX);
+    // }
   } else {
-    if (sortedCellsO.length <= 3) {
-      winAlgo(sortedCellsO);
-    } else if (sortedCellsO.length == 4) {
-      winAlgo(sortedCellsO.substring(0, 3));
-      winAlgo(sortedCellsO.substring(1, 3));
-    }
+    winAlgo(sortedCellsO);
+    console.log(arrayO);
+    console.log(sortedCellsO);
+    // if (sortedCellsO.length <= 3) {
+    //   winAlgo(sortedCellsO);
+    //   console.log(sortedCellsO);
+    // } else if (sortedCellsO.length == 4) {
+    //   // winAlgo(sortedCellsO.substring(0, 3));
+    //   // winAlgo(sortedCellsO.substring(1, 3));
+    //   console.log(sortedCellsO);
+    // }
   }
 };
 
 const winAlgo = (toCheck) => {
   winConditions.forEach((condition) => {
-    if (toCheck === condition) {
-      console.log("winner found!");
+    for (let i = 0; i < toCheck.length; i++) {
+      for (let j = 1; j < toCheck.length - 1; j++) {
+        let possibleCombination =
+          toCheck.charAt(i) + toCheck.charAt(j) + toCheck.charAt(j + 1);
+        console.log(possibleCombination);
+        if (possibleCombination == condition) {
+          console.log("winner found!");
+        }
+      }
     }
   });
 };
+
+// const algo = (string) => {
+//   for (let i = 0; i < string.length - 1; i++) {
+//     for (let j = 1; j < string.length - 2; j++) {
+//       let possibleCombination = (string.charAt(i) + string.charAt(j) + string.charAt(j + 1));
+//       if(toCheck == condition){
+//         console.log("winner found!");
+//       }
+//     }
+//   }
+// };
